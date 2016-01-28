1ef7893ac6625e29cf642622939e17c023fd6961
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-01-28 11:19:32 -0500

    Add support for custom fonts [stage-1]

--- test/e2e/settings.js
+++ test/e2e/settings.js
@@ -5,9 +5,10 @@
 
   var chai = require("chai");
   var chaiAsPromised = require("chai-as-promised");
+  var expect = chai.expect;
 
   chai.use(chaiAsPromised);
-  var expect = chai.expect;
+  browser.driver.manage().window().setSize(1024, 768);
 
   describe("Time and Date Settings - e2e Testing", function() {
 
@@ -15,33 +16,39 @@
       browser.get("/src/settings-e2e.html");
     });
 
-    it("Should load all components", function () {
-      // Widget Button Toolbar
+    it("Should load Save button", function () {
       expect(element(by.css("button#save")).isPresent()).to.eventually.be.true;
-      expect(element(by.css("button#cancel")).isPresent()).to.eventually.be.true;
-
-      // Font Setting
-      expect(element(by.css("#time-date-font .mce-tinymce")).isPresent()).to.eventually.be.true;
-
     });
 
-    it("Should correctly load default settings", function () {
-      // Show Time should be true
+    it("Should load Cancel button", function () {
+      expect(element(by.css("button#cancel")).isPresent()).to.eventually.be.true;
+    });
+
+    it("Should load Font Setting component", function () {
+      expect(element(by.css("#time-date-font .mce-tinymce")).isPresent()).to.eventually.be.true;
+    });
+
+    it("Should select Show Time", function () {
       expect(element(by.model("settings.additionalParams.showTime")).isSelected()).to.eventually.be.true;
+    });
 
-      // Time format should be h:mm A
+    it("Should set time format", function () {
       expect(element(by.model("settings.additionalParams.timeFormat")).getAttribute("value")).to.eventually.equal("h:mm A");
+    });
 
-      // Show Date should be true
+    it("Should select Show Date", function () {
       expect(element(by.model("settings.additionalParams.showDate")).isSelected()).to.eventually.be.true;
+    });
 
-      // Date format should be MMMM DD, YYYY
+    it("Should set date format", function () {
       expect(element(by.model("settings.additionalParams.dateFormat")).getAttribute("value")).to.eventually.equal("MMMM DD, YYYY");
+    });
 
-      // save button should be enabled
+    it("Should enable Save button", function () {
       expect(element(by.css("button#save[disabled=disabled")).isPresent()).to.eventually.be.false;
+    });
 
-      // form should be valid
+    it("Should set form to valid", function () {
       expect(element(by.css("form[name='settingsForm'].ng-invalid")).isPresent()).to.eventually.be.false;
     });
 
@@ -64,7 +71,8 @@
           "fontStyle":{
             "font": {
               "family": "verdana,geneva,sans-serif",
-              "type": "standard"
+              "type": "standard",
+              "url": ""
             },
             "size": "24px",
             "align": "left",
