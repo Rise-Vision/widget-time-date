8c99583b858f1221b77a3dfeffe39c2d69b3a0ff
santiagonoguez <santiago@nul-unu.com>
2018-07-24 15:07:18 -0500

    include GCS configuration and upload-dist

--- upload-dist.sh
+++ upload-dist.sh
@@ -1,7 +1,3 @@
-git clone git@github.com:Rise-Vision/private-keys.git
-mv private-keys ..
-gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ../private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
-
 cp -r dist time-date
 gsutil -m cp -r time-date gs://install-versions.risevision.com/widgets/
 gsutil -m acl -r ch -u AllUsers:R gs://install-versions.risevision.com/widgets/time-date
